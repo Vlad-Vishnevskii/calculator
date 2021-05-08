50c236ea9885ad948b7e74bc3ee5b09c61cd9fa4
Vlad <vishnevskii_vlad@mail.ru>
2021-05-08 21:48:27 +0300

    Добавляет обработчики нажатий на цифры + и -

--- build/js/script.js
+++ build/js/script.js
@@ -1,1 +1,91 @@
 const calc = document.querySelector('.calc');
+const calcOutput = document.querySelector('.calc__output');
+let numberClickCounter = 0;
+let arithmeticClickCounter = 0;
+let onSumButtonClickCounter = 0;
+let arithmeticResult = 0;
+let typeOffOperation = '';
+
+const onButtonClearClick = function (target) {
+  if (target.classList.contains('calc__button_clear')) {
+    calcOutput.textContent = 0;
+    numberClickCounter = 0;
+    arithmeticClickCounter = 0;
+    arithmeticResult = 0;
+  }
+}
+
+const onButtonNumberClick = function (target) {
+  if (target.classList.contains('calc__button_number')) {
+    numberClickCounter += 1;
+    if (numberClickCounter === 1) {
+      calcOutput.textContent = target.textContent;
+    } else {
+      calcOutput.textContent += target.textContent;
+    }
+  }
+}
+
+const summation = function () {
+  arithmeticResult += Number(calcOutput.textContent);
+}
+
+const subtraction = function () {
+  arithmeticResult -= Number(calcOutput.textContent);
+}
+
+const onSumButtonClick = function (target) {
+  if (target.classList.contains('calc__button_sum')) {
+    onSumButtonClickCounter += 1;
+    typeOffOperation = '+';
+    if (arithmeticClickCounter <= 1) {
+      arithmeticResult = Number(calcOutput.textContent);
+    } else {
+      arithmeticResult += Number(calcOutput.textContent);
+      calcOutput.textContent = arithmeticResult;
+    }
+  }
+}
+
+const onSubtractionButtonClick = function (target) {
+  if (target.classList.contains('calc__button_subtraction')) {
+    typeOffOperation = '-';
+    if (arithmeticClickCounter <= 1) {
+      arithmeticResult = Number(calcOutput.textContent);
+    } else {
+      arithmeticResult -= Number(calcOutput.textContent);
+      calcOutput.textContent = arithmeticResult;
+    }
+  }
+}
+
+const onArithmeticButtonClick = function (target) {
+  if (target.classList.contains('calc__button_arithmetic') && numberClickCounter > 0) {
+    arithmeticClickCounter += 1;
+    numberClickCounter = 0;
+    onSumButtonClick(target);
+    onSubtractionButtonClick(target);
+  }
+}
+
+const onButtonEqualsClick = function (target) {
+  if (target.classList.contains('calc__button_equal')) {
+    if (typeOffOperation === '+') {
+      summation();
+    }
+    if (typeOffOperation === '-') {
+      subtraction();
+    }
+    calcOutput.textContent = arithmeticResult;
+  }
+}
+
+const onButtonClick = function (evt) {
+  const target = evt.target;
+  onButtonNumberClick(target);
+  onButtonClearClick(target);
+  onArithmeticButtonClick(target);
+  onButtonEqualsClick(target);
+}
+
+calc.addEventListener('click',onButtonClick);
